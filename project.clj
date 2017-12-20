a475f63f7079895304be901587c475b0152ca9b2
ivan-yaroslavov <ivan.yaroslavov@ignorabilis.com>
2017-12-20 14:45:51 +0200

    Fix Loom incompatible with CLJS >= 1.9.854 by referencing a fork of cljs-priority-map

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject aysylu/loom "1.0.1-SNAPSHOT"
+(defproject ignorabilis/loom "1.0.1"
   :min-lein-version "2.0.0"
   :description "Graph library for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.priority-map "0.0.5"]
-                 [tailrecursion/cljs-priority-map "1.2.0"]]
+                 [ignorabilis/cljs-priority-map "1.2.1"]]
   :url "https://github.com/aysylu/loom"
   :test-selectors {:default (fn [m] (not (:test-check-slow m)))
                    :all (constantly true)
